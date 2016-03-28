a6e18620604342ec644610d4f9894ddf42c94b03
Tyson Omer <tysonomer@Tysons-MBP.ad.uvu.edu>
2016-03-28 11:22:39 -0600

    Chat Commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //This is where the JS magig happens
 
   // Add Ref to Firebase
+var messagesRef = new Firebase("https://tysonfirebasechat.firebaseio.com/");
 
   // Register the Dom Elaments with jQuery
   var messageField = $('#messageInput');
@@ -10,16 +11,44 @@
   // Listem for Enter press
   messageField.keypress(function (e) {
     if (e.keyCode == 13) {
+        
       //save data to firebase and clear the message field
 
+      var name = nameField.val();
+      var message = messageField.val();
+      
+      messagesRef.push({name: name, text: message});
+      
+      messageField.val('')
     }
   });
 
   // Add a callback that is triggered for each chat message.
 
-      // get the data
+messagesRef.on("child_added", function(snapshot, prevChildKey) {
+  var newMessage = snapshot.val();
+  // get the data
+  Console.log(newMessage);
 
-      // Create Elements for Message (<li> for messageElement and <strong class='example-chat-username'></strong> for nameElement)
+  var username = newMessage.name;
+  var message = newMessage.text;
+
+  // Create Elements for Message (<li> for messageElement and <strong class='example-chat-username'></strong> for nameElement)
+  
+  var messageElement = $("<li>");
+  var nameElement = $("<strong class='example-chat-username'></strong>");
+  
+  nameElement.text(username);
+  messageElement.text(message).prepend(nameElement);
+  
+  messageList.append(messageElement);
+  
+  messageList[0].scrollTop = messageList[0].scrollHeight;
+  
+});
+      
+
+      
 
       // Add the message to messageList
 
